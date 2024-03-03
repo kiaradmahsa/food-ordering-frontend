5f2ac221357c241cc7e069b5208c8679b0b4e3d3
mahsa <kiaradmahsa@gmail.com>
2024-03-03 17:53:05 +0000

    Create Navbar Template

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 const Header = () => {
   return(
@@ -8,6 +10,12 @@ const Header = () => {
                     className="text-3xl font-bold tracking-tight text-orange-500">
                 MernEats
             </Link>
+            <div className="md:hidden">
+              <MobileNav/>
+            </div>
+            <div className="hidden md:block">
+              <MainNav/>
+            </div>
         </div>
     </div>
   )
