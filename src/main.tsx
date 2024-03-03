740d98bb9f2463a80abd459bd30788cd874975f7
mahsa <kiaradmahsa@gmail.com>
2024-03-03 20:58:24 +0000

    Setup Auth0

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Auth0ProviderWithNavigate>
           <AppRoutes />
       </Auth0ProviderWithNavigate>
-      <AppRoutes></AppRoutes>
     </Router>
   </React.StrictMode>
 );
