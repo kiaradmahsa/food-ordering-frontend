af45ed13849ce012405a005612008ebe264751da
mahsa <kiaradmahsa@gmail.com>
2024-03-21 10:58:56 +0000

    update user profile request

--- src/api/MyUserApi.tsx
+++ src/api/MyUserApi.tsx
@@ -3,9 +3,10 @@ import { useMutation } from "react-query";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 type CreateUserRequest = {
-    auth0Id : string;
-    email : string;
-}
+  auth0Id: string;
+  email: string;
+};
+
 
 export const useCreateMyUser = () => {
     const { getAccessTokenSilently } = useAuth0();
@@ -39,3 +40,45 @@ export const useCreateMyUser = () => {
         isSuccess
     };
 };
+
+type UpdateMyUserRequest = {
+    name: string;
+    addressLine1: string;
+    city: string;
+    country: string;
+  };
+
+  export const useUpdateMyUser = () => {
+    const { getAccessTokenSilently } = useAuth0();
+  
+    const updateMyUserRequest = async (formData: UpdateMyUserRequest) => {
+      const accessToken = await getAccessTokenSilently();
+  
+      const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+        method: "PUT",
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+      },
+        body: JSON.stringify(formData),
+      });
+  
+      if (!response.ok) {
+        throw new Error("Failed to update user");
+      }
+  
+      return response.json();
+    };
+  
+    const {
+      mutateAsync: updateUser,
+      isLoading,
+      isSuccess,
+      error,
+      reset,
+    } = useMutation(updateMyUserRequest);
+  
+
+  
+    return { updateUser, isLoading };
+  };
