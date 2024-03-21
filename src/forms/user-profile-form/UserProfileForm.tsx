72a11c501521f639865b3085a8446811c05b7d7f
mahsa <kiaradmahsa@gmail.com>
2024-03-21 13:37:14 +0000

    Get user API + Pre-populate user profile form

--- src/forms/user-profile-form/UserProfileForm.tsx
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -1,10 +1,12 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { TypeOf, z } from "zod";
+import { z } from "zod";
 import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../../components/ui/form";
 import { Input } from "@/components/ui/input";
 import LoadingButton from "@/components/LoadingButton";
 import { Button } from "@/components/ui/button";
+import { User } from "@/types";
+import { useEffect } from "react";
 
 const formSchema  = z.object({
     email: z.string().optional(),
@@ -18,15 +20,21 @@ const formSchema  = z.object({
 export type UserFormData = z.infer<typeof formSchema>;
 
 type Props = {
+  currentUser: User;
   onSave: (userProfileData : UserFormData )=> void;
   isLoading: boolean;
 
 }
 
-const UserProfileForm = ({onSave, isLoading}: Props) => {
+const UserProfileForm = ({onSave, isLoading , currentUser}: Props) => {
   const form = useForm<UserFormData>({
     resolver: zodResolver(formSchema),
+    defaultValues: currentUser,
   });
+
+  useEffect(()=>{
+        form.reset(currentUser);
+  }, [currentUser, form]);
 
   return(
     <Form {...form}>
