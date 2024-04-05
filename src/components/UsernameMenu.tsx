99b2e9e193ed0d22441968eac78eebc89ffb4ed0
mahsa <kiaradmahsa@gmail.com>
2024-04-05 11:52:07 +0100

    create restaurant form

--- src/components/UsernameMenu.tsx
+++ src/components/UsernameMenu.tsx
@@ -4,23 +4,28 @@ import {
     DropdownMenuContent,
     DropdownMenuItem,
     DropdownMenuTrigger,
-  } from "./ui/dropdown-menu";
+} from "./ui/dropdown-menu";
 
 import { Separator } from "./ui/separator";
 import { CircleUserRound } from "lucide-react";
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 
-const UsernameMenu = () =>{
-    const {user , logout} = useAuth0();
-    
-    return(
+const UsernameMenu = () => {
+    const { user, logout } = useAuth0();
+
+    return (
         <DropdownMenu>
             <DropdownMenuTrigger className="flex items-center px-3 font-bold hover:text-orange-500 gap-2">
-                <CircleUserRound className="text-orange-500"/>
+                <CircleUserRound className="text-orange-500" />
                 {user?.email}
             </DropdownMenuTrigger>
             <DropdownMenuContent>
+                <DropdownMenuItem>
+                    <Link to="/manage-restaurant" className="font-bold hover:text-orange-500 py-2">
+                        Manage Restaurant
+                    </Link>
+                </DropdownMenuItem>
                 <DropdownMenuItem>
                     <Link to="/user-profile" className="font-bold hover:text-orange-500 py-2">
                         User Profile
@@ -29,8 +34,8 @@ const UsernameMenu = () =>{
                 <Separator />
                 <DropdownMenuItem>
                     <Button
-                     onClick={()=> logout()}
-                     className="flex flex-1 font-bold bg-orange-500">
+                        onClick={() => logout()}
+                        className="flex flex-1 font-bold bg-orange-500">
                         Log Out
                     </Button>
                 </DropdownMenuItem>
