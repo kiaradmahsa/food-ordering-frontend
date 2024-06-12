be4b406a0e9931171ef7da7199c882a572e23b23
mahsa <kiaradmahsa@gmail.com>
2024-06-12 14:41:19 +0100

    added ckeckout flow

--- src/forms/user-profile-form/UserProfileForm.tsx
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -23,10 +23,11 @@ type Props = {
   currentUser: User;
   onSave: (userProfileData : UserFormData )=> void;
   isLoading: boolean;
-
+  title?: string;
+  buttonText?: string;
 }
 
-const UserProfileForm = ({onSave, isLoading , currentUser}: Props) => {
+const UserProfileForm = ({onSave, isLoading , currentUser, title="User Profile" , buttonText="Submit"}: Props) => {
   const form = useForm<UserFormData>({
     resolver: zodResolver(formSchema),
     defaultValues: currentUser,
@@ -42,7 +43,7 @@ const UserProfileForm = ({onSave, isLoading , currentUser}: Props) => {
               className="space-y-4 bg-gray-50 rounded-lg md:p-10">
 
                 <div>
-                    <h2 className="text-2xl font-bold">User Profile Form</h2>
+                    <h2 className="text-2xl font-bold">{title}</h2>
                     <FormDescription>
                         View and change your profile information here
                     </FormDescription>
@@ -107,7 +108,7 @@ const UserProfileForm = ({onSave, isLoading , currentUser}: Props) => {
 
                 {
                     isLoading ? <LoadingButton/> : <Button type="submit" className="bg-orange-500">
-                        Submit
+                        {buttonText}
                     </Button>
                 }
                     
